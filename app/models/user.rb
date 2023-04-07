14da60900d3e8329f16ac95d07d7259dbd3e5efa
carlos herrera <carlosfernando.zh@gmail.com>
2023-04-06 20:52:51 -0600

    style for forms and password complexity validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  validate :password_complexity
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    errors.add :password, 'must include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
 end
