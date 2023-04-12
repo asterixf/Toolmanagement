21b8fbc615e8c256248d6c773c1b9b06d2ae35b5
carlos herrera <carlosfernando.zh@gmail.com>
2023-04-11 23:26:54 -0600

    style reset password page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,14 +9,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 
 # SMTP settings for gmail
-# config.action_mailer.smtp_settings = {
-#   :address              => "smtp.gmail.com",
-#   :port                 => 587,
-#   :user_name            => '',
-#   :password             => '',
-#   :authentication       => "plain",
-#   :enable_starttls_auto => true
-# }
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'freudenhub2@gmail.com',
+  :password             => 'vzjqnsssxoezwtjo',
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
