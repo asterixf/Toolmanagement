88347853c76ca9f0f067f553de76b8d7d3450ec1
carlos herrera <carlosfernando.zh@gmail.com>
2023-04-21 18:55:54 -0600

    fixed migration bug

--- db/migrate/20230421221438_add_cavity_reference_in_blockages.rb
+++
@@ -1,5 +0,0 @@
-class AddCavityReferenceInBlockages < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :blockages, :cavity, null: true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_221438) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_155731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_221438) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "cavity_id"
+    t.bigint "cavity_id", null: false
     t.index ["cavity_id"], name: "index_blockages_on_cavity_id"
     t.index ["tool_id"], name: "index_blockages_on_tool_id"
   end
