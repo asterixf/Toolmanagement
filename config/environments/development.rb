66268780446f63042818f5e2cc9aece9242ebc71
carlos herrera <carlosfernando.zh@gmail.com>
2023-04-26 10:54:43 -0600

    test mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,8 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => host , protocol: 'http' }
-
-# SMTP settings for gmail
-config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = {from: 'no-reply@freudenhub.com'}
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
