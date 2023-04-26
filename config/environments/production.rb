503304f583289ec1e10b2bb4ddb65dc5138328fc
carlos herrera <carlosfernando.zh@gmail.com>
2023-04-26 10:50:48 -0600

    test mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,9 +89,15 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.delivery_method = :smtp
-  host = 'fierce-meadow-53812.herokuapp.com'
-  config.action_mailer.default_url_options = { :host => host , protocol: 'http' }
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@freudenhub.com'}
 
 # SMTP settings for gmail
 config.action_mailer.delivery_method = :smtp
