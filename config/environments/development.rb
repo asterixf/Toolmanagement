e0bfe4d9cec86cca38bbbc46164c373158a04f38
carlos herrera <carlosfernando.zh@gmail.com>
2023-04-26 10:39:17 -0600

    mailer test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,14 +6,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.default_url_options = { :host => host , protocol: 'http' }
 
 # SMTP settings for gmail
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => 'freudenhub2@gmail.com',
-  :password             => 'vzjqnsssxoezwtjo',
+  :user_name            => ENV["EMAIL_ADDRESS"],
+  :password             => ENV["EMAIL_PASSWORD"],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
