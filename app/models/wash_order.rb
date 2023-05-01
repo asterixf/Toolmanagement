b1d040cefc6081e40a5369e10e063dc09050d0c2
carlos herrera <carlosfernando.zh@gmail.com>
2023-04-30 19:34:06 -0600

    wash_order formatted time

--- app/models/wash_order.rb
+++ app/models/wash_order.rb
@@ -3,4 +3,11 @@ class WashOrder < ApplicationRecord
   has_many :washables
   has_many :blockages, through: :washables
   validates :status, presence: true
+
+  def formatted_time
+    hours = time / 3600
+    minutes = (time % 3600) / 60
+    seconds = time % 60
+    format('%02d:%02d:%02d', hours, minutes, seconds)
+  end
 end
