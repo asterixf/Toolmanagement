19bb8bf07e34de5c4ff2c3bc9e9dcded08a2cfb4
carlos herrera <carlosfernando.zh@gmail.com>
2023-05-02 22:21:54 -0600

    fz05024

--- app/models/damage_report.rb
+++ app/models/damage_report.rb
@@ -2,5 +2,5 @@ class DamageReport < ApplicationRecord
   has_many :damages
   belongs_to :tool
   has_many :blockages, through: :damages
-  validates :machine_num, :shift, :operator, :bcl, :process_eng, :damaged_part, :description, :cause, presence: true
+  validates :quality, :machine_num, :shift, :operator, :bcl, :process_eng, :damaged_part, :description, :cause, presence: true
 end
